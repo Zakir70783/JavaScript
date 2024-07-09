f0dde589e90d02e9edb414d7135a997bc9fb54e0
Zakir70783 <zakirhussain70783@gmail.com>
2024-07-09 08:58:26 +0000

    contain dev done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,19 @@
 const accountId = 144553
 let accountEmail = "zakirhussain 1234@gmail.com"
 var accountPassword ="12345"
-accountCity ="Patna"
+accountCity ="Patna" 
 
-accountId = 2
+let accountState;
+// accountId = 2 // not allowed
 
-console.log(accountId)
+accountEmail = "zakspartan1234@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bangalore"
+
+/*
+prefer not to use var 
+because of scope issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
