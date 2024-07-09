d3532d41a24ec6e855336fa4bb4843a3192746cf
Zakir70783 <zakirhussain70783@gmail.com>
2024-07-09 09:32:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version 
+
+/* but today things have become so advance that we need not to mention use strict 
+but using it states taht you will use newer version syntax
+*/
+
+// alert("hello")
+// alert(3+3)    // we are using nodejs not web browser, so it will not work
+
+console.log(3+3); console.log("zakir"); /* prefer not to write like this
+the code must be readable
+and in js ; is not necessary to write 
+*/
+
+console.log(3+3)
+
+console.log("ZAKIR")
+
+let name = "Zakir"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint for large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
