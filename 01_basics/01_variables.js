c18ae47027583c5148e383becf3251c11be3ec57
Zakir70783 <zakirhussain70783@gmail.com>
2024-07-09 09:00:27 +0000

    contain dev done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -16,4 +16,4 @@ because of scope issue in block scope and functional scope
 */
 
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
