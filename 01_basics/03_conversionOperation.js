fcd929b338807b5d279327f9eec649c3d18203b8
Zakir70783 <zakirhussain70783@gmail.com>
2024-07-13 05:57:20 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,15 +10,43 @@
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "zak"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn)
+// let isLoggedIn = "zak"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn)
 
-// 1 => true 
-// "" => false
-// "zak" => true
+// // 1 => true 
+// // "" => false
+// // "zak" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// // *****************************  Operatios  **********************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = " zakir"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(((3+4) * 5)  % 3 )
+
+// console.log(+true) // this types of conversions not prefered on companies
+// console.log(+"")  //   ''
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+++gameCounter;
+console.log(gameCounter);
