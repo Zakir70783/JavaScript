3a2b95b0ce7be3c7064e2ca4ae14fcdc07072ded
Zakir70783 <zakirhussain70783@gmail.com>
2024-07-13 06:17:46 +0000

    comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(null > 0)   // this is comparison check
+// console.log(null == 0)  // this is comparison check
+// console.log(null >= 0)  // this is equality check
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+//line 1 to line 7 , this type of conversion is not preferred
+
+// "===" strict check 
+
+// console.log("2" = 2)
+console.log("2" == 2)
+console.log("2"===2)
